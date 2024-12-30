ce66ef461a04593e067d3ff6f6fd93ff5b593eb4
explorer271 <sarkarankur271@gmail.com>
2024-12-30 19:43:15 +0530

    Update transposition.c

--- weiss/transposition.c
+++ weiss/transposition.c
@@ -26,7 +26,7 @@
 #include "transposition.h"
 
 
-TranspositionTable TT = { .requestedMB = HASH_DEFAULT };
+TranspositionTable TT = { .requestedMB = 1 };
 
 
 // Probe the transposition table
@@ -85,12 +85,12 @@ static void *ThreadClearTT(void *voidThread) {
     int count = thread->count;
 
     // Logic for dividing the work taken from CFish
-    uint64_t twoMB  = 2 * 1024 * 1024;
+    uint64_t oneMB  = 1 * 1024 * 1024;
     uint64_t size   = TT.count * sizeof(TTBucket);
     uint64_t slice  = (size + count - 1) / count;
-    uint64_t blocks = (slice + twoMB - 1) / twoMB;
-    uint64_t begin  = MIN(size, index * blocks * twoMB);
-    uint64_t end    = MIN(size, begin + blocks * twoMB);
+    uint64_t blocks = (slice + oneMB - 1) / oneMB;
+    uint64_t begin  = MIN(size, index * blocks * oneMB);
+    uint64_t end    = MIN(size, begin + blocks * oneMB);
 
     memset(TT.table + begin / sizeof(TTBucket), 0, end - begin);
 
@@ -116,11 +116,11 @@ void InitTT() {
     if (TT.mem)
         free(TT.mem);
 
-    uint64_t bytes = TT.requestedMB * 1024 * 1024;
+    uint64_t bytes = 1 * 1024 * 1024;
 
 #if defined(__linux__)
-    // Align on 2MB boundaries and request Huge Pages
-    TT.mem = aligned_alloc(2 * 1024 * 1024, bytes);
+    // Align on 1MB boundaries and request Huge Pages
+    TT.mem = aligned_alloc(1 * 1024 * 1024, bytes);
     TT.table = (TTBucket *)TT.mem;
     madvise(TT.table, bytes, MADV_HUGEPAGE);
 #else
@@ -135,7 +135,7 @@ void InitTT() {
         exit(EXIT_FAILURE);
     }
 
-    TT.currentMB = TT.requestedMB;
+    TT.currentMB = 1;
     TT.count = bytes / sizeof(TTBucket);
 
     // Zero out the memory
